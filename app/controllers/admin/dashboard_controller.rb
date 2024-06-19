343ffc8c2812d8aae239bcb6e0a57f31b3118333
Jan Edmark Arieta <edarieta09@gmail.com>
2024-06-19 12:35:37 -0400

    adds auth for dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate
+
   def show
+    @product_count = Product.count
+    @category_count = Category.count
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
@@ -10,31 +11,41 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-
     if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+      redirect_to [:admin, @product], notice: 'Product was successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to [:admin, @product], notice: 'Product was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    @product = Product.find params[:id]
+    @product = Product.find(params[:id])
     @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
+    redirect_to admin_products_url, notice: 'Product was successfully destroyed.'
   end
 
   private
 
-  def product_params
-    params.require(:product).permit(
-      :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
-    )
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :quantity, :category_id, :image)
+  end
 end
